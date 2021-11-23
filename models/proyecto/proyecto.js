07ac21dfc442f638ba0babec8f15b3265f9797cf
soyBarto <jdtorresf@unal.edu.co>
2021-11-23 12:37:01 -0500

    Se le hizo Populate a los avances

--- models/proyecto/proyecto.js
+++ models/proyecto/proyecto.js
@@ -62,7 +62,17 @@ const projectSchema = new Schema(
         },
       },
     ],
+  },
+  {
+    toJSON: { virtuals: true }, // So `res.json()` and other `JSON.stringify()` functions include virtuals
+    toObject: { virtuals: true } // So `console.log()` and other functions that use `toObject()` include virtuals
   });
+
+projectSchema.virtual("avances", {
+  ref: 'Avance',
+  localField: '_id',
+  foreignField: 'proyecto',
+});
 
 const ProjectModel = model('Proyecto', projectSchema);
 
